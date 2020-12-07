795dc60e6846f364b181500abd25c6123c1079a6
Sergey <ya.sergey137@gmail.com>
2020-12-07 22:35:47 +0300

    Test output

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace Gift
     {
         static void Main(string[] args)
         {
+            Gift gift = new Gift();
+            gift.Add(new Cake("Cake",5.0,100,2));
+            gift.Add(new Candy("Candy",2.5,25,10));
+            gift.Add(new Gingerbread("Gingerbread",5.0,200,1));
+            gift.Add(new Lollipop("Lollipop",7.5, 75,5));
 
+            Console.WriteLine($"Total weight: {gift.GetTotalWeight()}");
+            Console.WriteLine(gift);
+            gift.SortByName();
+            Console.WriteLine(gift);
+            gift.SortBySugar();
+            Console.WriteLine(gift);
+            gift.SortByWeight();
+            Console.WriteLine(gift);
+            gift.SortByCount();
+            Console.WriteLine(gift);
         }
     }
 }
